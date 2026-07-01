e81dc91685ac62a53ec3e0fe2272ed3f0ddf2061
dzeko003 <berenismassamba@gmail.com>
2026-07-01 21:17:33 +0100

    ajout fichier editorconfig

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
-console.log("🚀 DevSetup - Environnement de développement initialisé");
+console.log("DevSetup - Environnement de développement initialisé");
 
-// Exemple simple pour tester nodemon
 let counter = 0;
 
 setInterval(() => {
   counter++;
-  console.log(`⏱️ Application en cours d'exécution... tick ${counter}`);
+  console.log(`Application en cours d'exécution... ${counter}`);
 }, 3000);
