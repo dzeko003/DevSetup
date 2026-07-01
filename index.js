a09877764ca2f601d232db7d7a5dc5ea879ddefd
dzeko003 <berenismassamba@gmail.com>
2026-07-01 21:14:25 +0100

    configuration de nodemon

---
+++ index.js
@@ -0,0 +1,9 @@
+console.log("🚀 DevSetup - Environnement de développement initialisé");
+
+// Exemple simple pour tester nodemon
+let counter = 0;
+
+setInterval(() => {
+  counter++;
+  console.log(`⏱️ Application en cours d'exécution... tick ${counter}`);
+}, 3000);
